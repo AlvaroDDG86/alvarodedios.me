a824bd84db2951a769dd9268b3e89bf4a697fdcb
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-04 20:54:01 +0100

    Add images for the projects

--- src/services/images-service.js
+++ src/services/images-service.js
@@ -1,10 +1,28 @@
 import words from '../../public/assets/images/projects/your-words.png'
 import rate from '../../public/assets/images/projects/light-rate.png'
+import player from '../../public/assets/images/projects/player.png'
+import voices from '../../public/assets/images/projects/voices.png'
+import pinia from '../../public/assets/images/projects/pinia.png'
+import alg from '../../public/assets/images/projects/alg.png'
+import football from '../../public/assets/images/projects/football.png'
+import alvaro from '../../public/assets/images/projects/alvaro.png'
+import color from '../../public/assets/images/projects/color.png'
+import hero from '../../public/assets/images/projects/hero.png'
+import calc from '../../public/assets/images/projects/calc.png'
 import notFound from '../../public/assets/images/projects/not-found.png'
 export function getImage(imageName) {
     const images = {
         words,
         rate,
+        player,
+        voices,
+        pinia,
+        alg,
+        football,
+        alvaro,
+        color,
+        hero,
+        calc,
         notFound
     }
     return images[imageName] ? images[imageName] : images.notFound
