11b526596caff3e589bf2f7ec60174913933fdd0
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-04 20:54:40 +0100

    background video

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,14 @@
 	import Work from './views/Work.svelte'
 	import Contact from './views/Contact.svelte'
 	import Projects from './views/Projects.svelte'
+	import video from '../public/assets/videos/video.mp4'
 	let name: string = 'world';
 	export let url = "";
 </script>
 <div class="container">
+	<video id="background-video" autoplay loop muted poster={video}>
+		<source src={video} type="video/mp4">
+	</video>
 	<Router url="{url}">
 		<Aside />
 		<Route path="about" component="{About}" />
@@ -21,6 +25,17 @@
 </div>
 
 <style lang="scss">
+#background-video {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
 .container {
 	position: relative;
 	max-width: 100vw;
