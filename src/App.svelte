35e7b38ee8b891694086abae17b3ca6e7f27a79a
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-07 20:05:17 +0100

    add new bg

--- src/App.svelte
+++ src/App.svelte
@@ -7,13 +7,14 @@
 	import Contact from './views/Contact.svelte'
 	import Projects from './views/Projects.svelte'
 	import video from '../public/assets/videos/video.mp4'
+	import bg from '../public/assets/images/bg.jpg'
 	let name: string = 'world';
 	export let url = "";
 </script>
-<div class="container">
-	<video id="background-video" autoplay loop muted poster={video}>
+<div class="container" style="background-image: url({bg})">
+	<!-- <video id="background-video" autoplay loop muted poster={video}>
 		<source src={video} type="video/mp4">
-	</video>
+	</video> -->
 	<Router url="{url}">
 		<Aside />
 		<Route path="about" component="{About}" />
@@ -25,22 +26,26 @@
 </div>
 
 <style lang="scss">
-#background-video {
-  width: 100vw;
-  height: 100vh;
-  object-fit: cover;
-  position: fixed;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  z-index: -1;
-}
+// #background-video {
+//   width: 100vw;
+//   height: 100vh;
+//   object-fit: cover;
+//   position: fixed;
+//   left: 0;
+//   right: 0;
+//   top: 0;
+//   bottom: 0;
+//   z-index: -1;
+// }
 .container {
 	position: relative;
 	max-width: 100vw;
 	min-height: 100vh;
 	overflow: hidden;
+	background-attachment: fixed;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
 }
 // * {
 //     outline: 1px solid #f00 !important;
