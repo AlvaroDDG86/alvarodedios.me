0348eb4075ac6d445d2309b12170e2b3146f5c5f
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-24 19:15:46 +0100

    Add ip tracker project

--- src/services/images-service.js
+++ src/services/images-service.js
@@ -12,6 +12,7 @@ import calc from '../../public/assets/images/projects/calc.png'
 import fifty from '../../public/assets/images/projects/fifty.png'
 import rick from '../../public/assets/images/projects/rick.png'
 import users from '../../public/assets/images/projects/users.png'
+import ip from '../../public/assets/images/projects/ip.png'
 import notFound from '../../public/assets/images/projects/not-found.png'
 export function getImage(imageName) {
     const images = {
@@ -29,6 +30,7 @@ export function getImage(imageName) {
         fifty,
         rick,
         users,
+        ip,
         notFound
     }
     return images[imageName] ? images[imageName] : images.notFound
